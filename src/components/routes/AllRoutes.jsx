a3d6db36f0a350e153910728cab24d0f8586208d
bableshaazad <bableshaazad@outlook.com>
2024-07-13 11:33:15 +0530

    role based route is done

--- src/components/routes/AllRoutes.jsx
+++ src/components/routes/AllRoutes.jsx
@@ -1,5 +1,5 @@
 import { Suspense, useContext } from "react"
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import App from "../../app/App"
 import OptVerification from "../auth/OptVerification"
 import UserOtpVerifiedPage from "../auth/UserOtpVerifiedPage"
@@ -9,12 +9,9 @@ import HomePage from "../navbarpage/HomePage"
 import { RouteComps } from "./AllComponents.jsx"
 import ProtectOtpRoute from "../authprovider/ProtectOtpRoute.jsx"
 import { AuthContext } from "../authprovider/AuthProvider.jsx"
-import ProtectedRoute from "../authprovider/ProtectedRoute.jsx"
-import AfterLoginProtectComp from "../authprovider/AfterLoginProtectComp.jsx"
 
 function AllRoutes() {
     const { isLogin } = useContext(AuthContext);
-    const navigate = useNavigate();
 
     console.log(isLogin)
     return (
@@ -22,15 +19,6 @@ function AllRoutes() {
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route path="" element={<HomePage />} />
-
-                    {/* If User is loggedIn then not able to seen that components */}
-                    {/* {AfterLogC.map(({ comp, urlC }, index) => {
-                        <Route key={index} path={urlC} element={
-                            <AfterLoginProtectComp>
-                                {comp}
-                            </AfterLoginProtectComp>
-                        } />
-                    })} */}
 
                     {/* That component is visible only at the time of otp verification */}
                     <Route path="opt-verification" element={<ProtectOtpRoute>
@@ -40,15 +28,6 @@ function AllRoutes() {
                     <Route path="user-otp-verified-page" element={<ProtectOtpRoute>
                         <UserOtpVerifiedPage />
                     </ProtectOtpRoute>} />
-
-                    {/*! Protected Routes -> user is seen after login */}
-                    {/* {ProtectedC.map(({ comp, urlC }, index) => (
-                        <Route key={index} path={urlC} element={
-                            <ProtectedRoute>
-                                {comp}
-                            </ProtectedRoute>
-                        } />
-                    ))} */}
 
                     {RouteComps.map(({ element, path, isPrivate, isVisibleAfterLogin, role }, index) => {
                         if (isLogin) {
