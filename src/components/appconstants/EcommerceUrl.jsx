f1753ab7af0ecb228ef836e1baf00d88a8fcb0dc
bableshaazad <bableshaazad@outllok.com>
2024-08-24 00:10:57 +0530

    change base url

--- src/components/appconstants/EcommerceUrl.jsx
+++ src/components/appconstants/EcommerceUrl.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://ecommerceshoppingapp-production.up.railway.app/api/v1/"
+export const BASE_URL = "https://ecommerce-shopping-app-bcsb.onrender.com/api/v1/"
 
 export const LOCAL_BASE_URL = "http://localhost:8080/api/v1/"
